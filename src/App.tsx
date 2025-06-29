183c8c80d26c85132ff78548e0acb1f23efa0360
Hector Pacheco <jozcar@hotmail.com>
2025-06-29 15:08:18 -0400

    Added vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import MainLayout from "./layout/MainLayout";
 import AppRoutes from "./routes/AppRoutes";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
     return (
         <div>
             <MainLayout>
                 <AppRoutes />
+               <Analytics />
             </MainLayout>
         </div>
     )
